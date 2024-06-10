347e5eb633ecbabf5e7d30185c0160eedf8077fd
roncamposj <roncamposj@knights.ucf.edu>
2024-06-10 17:48:41 -0400

    .

--- video_chatgpt/train/train_mem.py
+++ video_chatgpt/train/train_mem.py
@@ -1,5 +1,5 @@
 # Need to call this before importing transformers.
-from video_chatgpt.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
+#from video_chatgpt.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 
 #replace_llama_attn_with_flash_attn()
 
